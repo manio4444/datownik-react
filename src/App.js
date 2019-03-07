2e1f4403e7e2c973e54118f30ce5167d4cb6e671
Marcin Kalinowski <m.kalinowski@intive.com>
2019-03-07 13:40:07 +0100

    ws

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './css/reset.css';
 import './css/main.css';
 import PageStart from './PageStart';
 import MainMenu from './MainMenu';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <MainMenu />
-        </header>
-        <PageStart />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <MainMenu/>
+                </header>
+                <PageStart/>
+            </div>
+        );
+    }
 }
 
 export default App;

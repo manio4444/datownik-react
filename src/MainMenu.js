1cb46f7f374edb93a58c0336cc6d33515a0e9c88
Marcin Kalinowski <m.kalinowski@intive.com>
2019-03-07 11:53:04 +0100

    hamburger click event + css fixes

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import './MainMenu.css';
 
 class MainMenu extends Component {
+    openState = () => {
+        console.log(this.state.opened);
+
+        this.setState({
+            opened: !this.state.opened,
+        });
+    };
     state = {
         opened: false
     };
@@ -9,23 +16,23 @@ class MainMenu extends Component {
     render() {
         return (
             <div className="MainMenu">
-                <button type="button" className="hamburger"><span className="hamburger_line"></span></button>
-                <header>
-                    <nav>
-                        <button type="button" className="hamburger_close"><span className="hamburger_line"></span></button>
-                        <ul>
-                            <li><a href="<?php $explode = explode('?', $_SERVER['REQUEST_URI'], 2); echo $explode[0]; ?>">Main</a></li>
-                            <li><a href="?page=kalendarz">Kalendarz</a></li>
-                            <li><a href="?page=zakladki">Zakladki</a></li>
-                            <li><a href="?page=notatki">Notatki</a></li>
-                            <li><a href="?page=dokumenty">Dokumenty</a></li>
-                            <li><a href="?page=do-zrobienia">To do</a></li>
-                            <li><a href="?page=kontakty">Kontakty</a></li>
-                            <li><a href="?page=pliki">Pliki</a></li>
-                            <li><a href="?page=ustawienia">Ustawienia</a></li>
-                        </ul>
-                    </nav>
-                </header>
+                <button type="button" className="hamburger" onClick={this.openState}><span className="hamburger_line"></span></button>
+                <div className="MainMenuWrapper">
+                <nav>
+                    <button type="button" className="hamburger_close"><span className="hamburger_line"></span></button>
+                    <ul>
+                        <li><a href="<?php $explode = explode('?', $_SERVER['REQUEST_URI'], 2); echo $explode[0]; ?>">Main</a></li>
+                        <li><a href="?page=kalendarz">Kalendarz</a></li>
+                        <li><a href="?page=zakladki">Zakladki</a></li>
+                        <li><a href="?page=notatki">Notatki</a></li>
+                        <li><a href="?page=dokumenty">Dokumenty</a></li>
+                        <li><a href="?page=do-zrobienia">To do</a></li>
+                        <li><a href="?page=kontakty">Kontakty</a></li>
+                        <li><a href="?page=pliki">Pliki</a></li>
+                        <li><a href="?page=ustawienia">Ustawienia</a></li>
+                    </ul>
+                </nav>
+                </div>
             </div>
         );
     }

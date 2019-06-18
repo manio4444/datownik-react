74fa8f5eef2402b25eba899c19813cf4cc22ef6e
Marcin Kalinowski <m.kalinowski@intive.com>
2019-06-18 16:24:34 +0200

    multicontent add prototype of todo modal + change loading options to dropdown

--- src/Multicontent.js
+++ src/Multicontent.js
@@ -1,31 +1,102 @@
 import React, {Component} from 'react';
 import { Form, TextArea, Dropdown } from "semantic-ui-react";
+import ModalTodoAdd from './ModalTodoAdd';
 import 'semantic-ui-css/components/form.min.css';
 import 'semantic-ui-css/components/dropdown.min.css';
 import 'semantic-ui-css/components/transition.min.css';
 import './Multicontent.scss';
 
 class Multicontent extends Component {
+    state = {
+        openModalTodoAdd: false,
+    };
+
+    handleOption = (e, {action}) => this.setState({[action]: true});
+
     render() {
+
+        const {openModalTodoAdd} = this.state;
+        const options = [
+            {
+                text: "Notatka",
+                icon: 'sticky note outline',
+                description: 'Not ready',
+                action: 'openModalNoteAdd'
+            },
+            {
+                text: "Zadanie",
+                icon: 'calendar check outline',
+                action: 'openModalTodoAdd'
+            },
+            {divider: true},
+            {
+                text: "Wydarzenie",
+                icon: 'calendar outline',
+                description: 'Not ready',
+                action: 'openModalEventAdd'
+            },
+            {
+                text: "Zakładka",
+                icon: 'external',
+                description: 'Not ready',
+                action: 'openModalBookmarkAdd'
+            },
+            {
+                text: "Dokument",
+                icon: 'question',
+                description: 'Not ready',
+                action: 'openModalDocAdd'
+            },
+            {
+                text: "Kontakt",
+                icon: 'question',
+                description: 'Not ready',
+                action: 'openModalContactAdd'
+            },
+            {
+                text: "Kod/hasło",
+                icon: 'question',
+                description: 'Not ready',
+                action: 'openModalPassAdd'
+            },
+        ];
+
         return (
-            <Form method="POST" className="multicontent__form">
+            <React.Fragment>
+                <Form method="POST" className="multicontent__form">
 
-                <TextArea className='multicontent__textarea icon' name="urladd" rows="5"/>
+                    <TextArea
+                        className='multicontent__textarea icon'
+                        name="urladd"
+                        rows="2"
+                    />
 
-                <Dropdown className='multicontent__button' text='Dodaj nowy' button>
-                    <Dropdown.Menu>
-                        <Dropdown.Item text="Notatka" icon='sticky note outline'/>
-                        <Dropdown.Divider/>
-                        <Dropdown.Item text="Zadanie" icon='calendar check outline' description='Not ready'/>
-                        <Dropdown.Item text="Wydarzenie" icon='calendar outline' description='Not ready'/>
-                        <Dropdown.Item text="Zakładka" icon='external' description='Not ready'/>
-                        <Dropdown.Item text="Dokument" icon='question' description='Not ready'/>
-                        <Dropdown.Item text="Kontakt" icon='question' description='Not ready'/>
-                        <Dropdown.Item text="Kod/hasło" icon='question' description='Not ready'/>
-                    </Dropdown.Menu>
-                </Dropdown>
+                    <Dropdown className='multicontent__button' text='Dodaj nowy' button>
+                        <Dropdown.Menu>
+                            {options.map((option, i) => {
+                                if (option.divider === true) return (<Dropdown.Divider key={i}/>);
+                                return (
+                                    <Dropdown.Item
+                                        key={i}
+                                        text={option.text}
+                                        icon={option.icon}
+                                        description={option.description}
+                                        action={option.action}
+                                        onClick={this.handleOption}
+                                    />
+                                );
+                            })}
+                        </Dropdown.Menu>
+                    </Dropdown>
 
-            </Form>
+                </Form>
+
+                {openModalTodoAdd && <ModalTodoAdd
+                    open={openModalTodoAdd}
+                    trueCallback={() => console.log('trueCallback')}
+                    falseCallback={() => this.setState({openModalTodoAdd: false})}
+                />}
+            </React.Fragment>
         );
     }
 }

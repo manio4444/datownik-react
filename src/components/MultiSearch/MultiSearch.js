6b34db7378f87e8d723c6ccbe1a3206540a4b986
manio4444 <kalinowskixmarcin@o2.pl>
2019-11-02 01:04:54 +0100

    MultiSearch working API query

--- src/components/MultiSearch/MultiSearch.js
+++ src/components/MultiSearch/MultiSearch.js
@@ -1,21 +1,55 @@
 import React, {Component} from 'react';
 import {Input} from "semantic-ui-react";
 import './MultiSearch.scss';
+import axios from "axios";
 
 class MultiSearch extends Component {
     state = {
         searchInput: '',
+        searchDelay: 500,
+        searchMinLength: 3,
+        isLoading: false,
     };
 
-    handleChange = (e, {name, value}) => this.setState({ [name]: value });
+    inputSearch = (e, {value}) => {
+        this.setState({searchInput: value});
 
-    onSearch = () => {
-        console.log('onSearch');
+        clearTimeout(this.searchTimeout);
+
+        this.searchTimeout = setTimeout(() => {
+            if (this.state.searchInput.length < this.state.searchMinLength) {
+                console.log('clear search');
+                return;
+            }
+            this.doSearch();
+        }, this.state.searchDelay);
+    };
+
+    doSearch = () => {
+        clearTimeout(this.searchTimeout);
+
+        this.setState({isLoading: true});
+
+        axios.post(process.env.REACT_APP_ENDPOINT_URL, {
+            ajax_action: 'multisearchAjax',
+            operation: 'getData',
+            txt: this.state.searchInput,
+        })
+            .then(res => {
+                console.log('API response');
+                console.log(res);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+            .finally(() => {
+                this.setState({isLoading: false});
+            });
     };
 
     render() {
 
-        const {searchInput} = this.state;
+        const {searchInput, isLoading} = this.state;
 
         return (
             <React.Fragment>
@@ -26,11 +60,13 @@ class MultiSearch extends Component {
                     iconPosition='left'
                     action={{
                         content: 'Szukaj',
-                        onClick: this.onSearch,
+                        onClick: this.doSearch,
+                        loading: isLoading,
+                        disabled: isLoading,
                     }}
-                    name={'searchInput'}
+                    disabled={isLoading}
                     value={searchInput}
-                    onChange={this.handleChange}
+                    onChange={this.inputSearch}
                 />
             </React.Fragment>
         );

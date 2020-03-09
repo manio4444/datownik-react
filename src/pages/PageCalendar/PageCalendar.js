9ff466d8cb1dd9fe95832afd11fb2daf6e10eca8
manio4444 <kalinowskixmarcin@o2.pl>
2020-03-09 01:23:27 +0100

    WIP: PageCalendar base table of month

--- src/pages/PageCalendar/PageCalendar.js
+++ src/pages/PageCalendar/PageCalendar.js
@@ -1,16 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import moment from "moment";
 
-class PageCalendar extends Component {
+const DayOfMonth = ({day}) => {
+    return <div
+        style={{
+            width: 'calc(100% / 7)',
+            border: '1px solid lightgray'
+        }}
+        className="calendar__days"
+    >{day}</div>
+};
 
-    render() {
+class CalendarTable extends Component {
+
+    daysOfMonth = date => {
+        const days = [];
+        const daysInMonth = Number(date.daysInMonth());
+        let dateDay = Number(date.startOf('month').format('D'));
+
+        while (dateDay <= daysInMonth) {
+            days.push(dateDay);
+            dateDay++;
+        }
+        return days
+    };
+
+    daysOfPreviousMonth = date => {
+        const prevDate = date.subtract(1, 'months'); //temp
+        const daysInPrevMonth = Number(date.daysInMonth());
+        const endOfPrevMonth = prevDate.endOf('month');
+        const endWeekdayOfPrevMonth = Number(endOfPrevMonth.isoWeekday());
+        const prevMonthDays = [];
+        let dateDay = daysInPrevMonth - endWeekdayOfPrevMonth + 1;
+
+        if (endWeekdayOfPrevMonth === 7) return [];
+
+        while (dateDay <= daysInPrevMonth) {
+            prevMonthDays.push(dateDay);
+            dateDay++;
+        }
+        console.log(prevMonthDays);
+        return prevMonthDays
+    };
+
+    render () {
+        const daysToShow = [
+            ...this.daysOfPreviousMonth(moment()),
+            ...this.daysOfMonth(moment()),
+            // ...this.daysOfNextMonth(moment()),
+        ];
+
         return (
-        <React.Fragment>
-            <section className="calendar">
+            <React.Fragment>
+                <br/>
+                <div style={{display: 'flex', 'flex-wrap': 'wrap'}}>
+                    {daysToShow.map(day => <DayOfMonth day={day} />)}
+                </div>
 
-            </section>
-        </React.Fragment>
+            </React.Fragment>
         );
     }
 }
+
+
+const PageCalendar = () => {
+    return (
+        <React.Fragment>
+            <section className="calendar">
+
+                <CalendarTable />
+
+            </section>
+        </React.Fragment>
+    );
+};
 
 export default PageCalendar;

ea6bb7b990fc719bb742d06031028038dd048cdd
manio4444 <kalinowskixmarcin@o2.pl>
2020-09-08 22:57:15 +0200

    CalendarTable base ready (added next month days + some refactor + cleaning)

--- src/components/Calendar/CalendarTable.js
+++ src/components/Calendar/CalendarTable.js
@@ -1,62 +1,64 @@
 import React, { Component } from 'react';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Table } from 'semantic-ui-react'
 
-const DayOfMonth = ({day}) => {
-    return <div
-        style={{
-            width: 'calc(100% / 7)',
-            border: '1px solid lightgray'
-        }}
-        className="calendar__days"
-    >{day}</div>
-};
+const daysOfWeek = [
+    'Poniedziałek',
+    'Wtorek',
+    'Środa',
+    'Czwartek',
+    'Piątek',
+    'Sobota',
+    'Niedziela',
+];
 
 class CalendarTable extends Component {
 
-    daysOfWeek = [
-        'Poniedziałek',
-        'Wtorek',
-        'Środa',
-        'Czwartek',
-        'Piątek',
-        'Sobota',
-        'Niedziela',
-    ];
-
     isLastWeekDay = day => Number(day.isoWeekday()) === 7;
+
+    isFirstWeekDay = day => Number(day.isoWeekday()) === 1;
 
     currentMonth = date => {
         const days = [];
-        let dateDay = date.clone().startOf('month');
+        let day = date.clone().startOf('month');
 
-        while (dateDay.isSame(date, 'month')) {
+        while (day.isSame(date, 'month')) {
             days.push({
-                id: dateDay.format('YMD'),
-                day: dateDay.format('D'),
+                iso: day.format(),
+                day: day.format('D'),
             });
-            dateDay.add(1, 'day');
+            day.add(1, 'day');
         }
         return days;
     };
 
     previousMonthWeek = date => {
-        const lastDay = date.endOf('month');
-        const lastWeekday = Number(lastDay.isoWeekday());
-
-        if (this.isLastWeekDay(lastDay)) return [];
-
+        let day = date.endOf('month');
         const prevMonthDays = [];
-        let day = lastDay.clone().subtract(lastWeekday - 1, 'days');
 
-        while (day.isSame(date, 'month')) {
-            prevMonthDays.push({
-                id: day.format('YMD'),
+        while (!this.isLastWeekDay(day)) {
+            prevMonthDays.unshift({
+                iso: day.format(),
+                day: day.format('D'),
+            });
+            day.subtract(1, 'day');
+        }
+
+        return prevMonthDays;
+    };
+
+    nextMonthWeek = date => {
+        let day = date.startOf('month');
+        const nextMonthDays = [];
+
+        while (!this.isFirstWeekDay(day)) {
+            nextMonthDays.push({
+                iso: day.format(),
                 day: day.format('D'),
             });
             day.add(1, 'day');
         }
 
-        return prevMonthDays;
+        return nextMonthDays;
     };
 
     daysIntoRows = days => {
@@ -77,36 +79,33 @@ class CalendarTable extends Component {
     render () {
         const {date} = this.props;
         const previousMonth = date.clone().subtract(1, 'months');
+        const nextMonth = date.clone().add(1, 'months');
 
         const calendarDays = [
             ...this.previousMonthWeek(previousMonth),
             ...this.currentMonth(date),
-            // ...this.nextMonthWeek(nextMonth),
+            ...this.nextMonthWeek(nextMonth),
         ];
         const calendarRows = this.daysIntoRows(calendarDays);
 
         return (
-            <React.Fragment>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        {daysOfWeek.map(name => <Table.HeaderCell key={name}>
+                            {name}
+                        </Table.HeaderCell>)}
+                    </Table.Row>
+                </Table.Header>
 
-                <Table celled>
-                    <Table.Header>
-                        <Table.Row>
-                            {this.daysOfWeek.map(name => <Table.HeaderCell key={name}>
-                                {name}
-                            </Table.HeaderCell>)}
-                        </Table.Row>
-                    </Table.Header>
-
-                    <Table.Body>
-                        {calendarRows.map((row, i) => <Table.Row key={i}>
-                            {row.map(day => <Table.Cell key={day.id}>
-                                {day.day}
-                            </Table.Cell>)}
-                        </Table.Row>)}
-                    </Table.Body>
-                </Table>
-
-            </React.Fragment>
+                <Table.Body>
+                    {calendarRows.map((row, i) => <Table.Row key={i}>
+                        {row.map(day => <Table.Cell key={day.iso}>
+                            {day.day}
+                        </Table.Cell>)}
+                    </Table.Row>)}
+                </Table.Body>
+            </Table>
         );
     }
 }

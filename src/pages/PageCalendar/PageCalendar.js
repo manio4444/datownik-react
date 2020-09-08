81b7a8f87b8853f7bba4c9c485221d07180af5aa
manio4444 <kalinowskixmarcin@o2.pl>
2020-09-08 23:35:00 +0200

    PageCalendar add date manipulation buttons and month title + year

--- src/pages/PageCalendar/PageCalendar.js
+++ src/pages/PageCalendar/PageCalendar.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from "moment";
+import { Grid, Button } from "semantic-ui-react";
 import CalendarTable from "../../components/Calendar/CalendarTable";
+import 'semantic-ui-css/components/grid.min.css'; // TODO - import this in specific component
+
+const monthsOfYear = [
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzień',
+];
 
 const PageCalendar = () => {
+    const [date, setDate] = useState(moment());
+
+    const month = monthsOfYear[date.format('M') - 1];
+    const year = date.format('YYYY');
+
+    const handleSetDate = type => {
+        if (type === 'subtract') setDate(date.clone().subtract(1, 'month'));
+        if (type === 'add') setDate(date.clone().add(1, 'month'));
+    };
+
     return (
         <React.Fragment>
             <section className="calendar">
 
-                <CalendarTable date={moment('2020-03-03')} />
+                <h1>{month} {year}</h1>
+
+                <Grid>
+                    <Grid.Column width={8}>
+                        <Button
+                            onClick={() => handleSetDate('subtract')}
+                        >
+                            Poprzedni
+                        </Button>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Button
+                            onClick={() => handleSetDate('add')}
+                            floated={'right'}
+                        >
+                            Następny
+                        </Button>
+                    </Grid.Column>
+                </Grid>
+
+                <CalendarTable date={date}/>
 
             </section>
         </React.Fragment>

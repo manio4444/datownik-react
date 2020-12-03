c325b25050c17439f2688db6dd7e16e8413a8a69
manio4444 <kalinowskixmarcin@o2.pl>
2020-12-03 17:07:16 +0100

    SearchNotes add clearing results icon

--- src/components/Notes/SearchNotes.js
+++ src/components/Notes/SearchNotes.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import { Input } from "semantic-ui-react";
+import React  from 'react';
+import { Input, Icon } from "semantic-ui-react";
 
 const SearchNotes = ({searchQuery, setSearchQuery}) => {
-    const [value, setValue] = useState(searchQuery);
 
-    const handleSetValue = ({target}) => {
-        setValue(target.value);
-        setSearchQuery(target.value);
-    };
+    const handleSetValue = ({target}) => setSearchQuery(target.value);
+
+    const handleClearValue = () => setSearchQuery('');
 
     return <div className={'notes_search'}>
         <Input
             fluid
             placeholder='Szukaj notatek'
-            icon={'search'}
+            icon={<Icon
+                name={searchQuery ? 'close' : 'search'}
+                link={searchQuery}
+                onClick={handleClearValue}
+            />}
             iconPosition='left'
-            value={value}
+            value={searchQuery}
             onChange={handleSetValue}
         />
     </div>

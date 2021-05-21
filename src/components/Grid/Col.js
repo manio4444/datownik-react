f0b94d85c061583a4e1aa6b61e7cdf7053bd5e6c
manio4444 <kalinowskixmarcin@o2.pl>
2021-05-21 12:32:51 +0200

    Page start add link to facebook events + create Col component

---
+++ src/components/Grid/Col.js
@@ -0,0 +1,11 @@
+import React  from 'react';
+
+import '../../assets/scss/page.scss';
+
+const Col = ({size, className, children}) => (
+    <div className={`${className} ${size && `page__grid-${size}`}`}>
+        {children}
+    </div>
+);
+
+export default Col;

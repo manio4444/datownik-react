77b8a26c7120730a71e50608ba40a985639a2152
manio4444 <kalinowskixmarcin@o2.pl>
2021-05-25 01:23:05 +0200

    CalendarDay add Link to all not offset days for further routing

--- src/components/Calendar/CalendarDay.js
+++ src/components/Calendar/CalendarDay.js
@@ -1,19 +1,37 @@
 import React  from 'react';
+import { Link } from 'react-router-dom';
 
+import { RouterPaths } from "../../router/consts";
 import Placeholder from "../Placeholder/Placeholder";
 
-const CalendarDay = ({day, events, loading}) => <div
-    className={`calendar-table__day ${day.offset ? 'calendar-table__day--offset' : ''}`}
->
-    <span className={'calendar-table__day-number'}>{day.day}</span>
-    {!loading && events.map(event => <p
-        key={`${event.type}-${event.id}`}
-        className={`calendar-table__day-event calendar-table__day-event--${event.type}`}
-    >
-        {event.txt}
-    </p>)}
-    {loading && <Placeholder />}
-    {loading && <Placeholder />}
-</div>;
+const CalendarDay = ({day, events, loading}) => {
+
+    const offsetDay = !!day.offset;
+
+    const CalendarDayContent = <>
+        <span className={'calendar-table__day-number'}>{day.day}</span>
+        {!loading && events.map(event => <p
+            key={`${event.type}-${event.id}`}
+            className={`calendar-table__day-event calendar-table__day-event--${event.type}`}
+        >
+            {event.txt}
+        </p>)}
+        {loading && <Placeholder/>}
+        {loading && <Placeholder/>}
+    </>;
+
+    return offsetDay ? (
+        <div className={`calendar-table__day calendar-table__day--offset`}>
+            {CalendarDayContent}
+        </div>
+    ) : (
+        <Link
+            to={`/${RouterPaths.CALENDAR}/${day.iso}`}
+            className={`calendar-table__day`}
+        >
+            {CalendarDayContent}
+        </Link>
+    )
+};
 
 export default CalendarDay;

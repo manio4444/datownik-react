fa92cfd65770479d3ea3ab8257b30c8390c83606
manio4444 <kalinowskixmarcin@o2.pl>
2021-06-14 16:39:31 +0200

    Add ModalEventAdd to PageCalendar and button to handle opening it

--- src/pages/PageCalendar/PageCalendar.js
+++ src/pages/PageCalendar/PageCalendar.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import moment from "moment";
-import { Grid, Button } from "semantic-ui-react";
+import { Grid, Button, Icon } from "semantic-ui-react";
 
 import { getMonthEvents } from "../../components/Calendar/actions"; //TODO - move to component inside /components/Calendar/
 import CalendarTable from "../../components/Calendar/CalendarTable";
 import { LANG_ARR_MONTHS } from '../../components/Calendar/const';
+import ModalEventAdd from "../../components/Calendar/ModalEventAdd";
 
-import 'semantic-ui-css/components/grid.min.css'; // TODO - import this in specific component
+import 'semantic-ui-css/components/grid.min.css';
 
 const PageCalendar = () => {
     const [date, setDate] = useState(moment());
     const [loadingEvents, setLoadingEvents] = useState(false);
     const [monthEvents, setMonthEvents] = useState([]);
+    const [modalEventAdd, setModalEventAdd] = useState(false);
 
     const month = Number(date.format('M'));
     const monthTitle = LANG_ARR_MONTHS[month - 1];
@@ -37,20 +39,32 @@ const PageCalendar = () => {
 
                 <h1>{monthTitle} {year}</h1>
 
-                <Grid>
-                    <Grid.Column width={8}>
+                <Grid columns={3}>
+                    <Grid.Column>
                         <Button
                             onClick={() => handleSetDate('subtract')}
                         >
-                            Poprzedni
+                            {'<'}
                         </Button>
                     </Grid.Column>
-                    <Grid.Column width={8}>
+
+                    <Grid.Column textAlign={'center'}>
+                        <Button
+                            icon
+                            labelPosition='left'
+                            onClick={() => setModalEventAdd(true)}
+                        >
+                            <Icon name='plus'/>
+                            Dodaj
+                        </Button>
+                    </Grid.Column>
+
+                    <Grid.Column>
                         <Button
                             onClick={() => handleSetDate('add')}
                             floated={'right'}
                         >
-                            Następny
+                            {'>'}
                         </Button>
                     </Grid.Column>
                 </Grid>
@@ -62,6 +76,13 @@ const PageCalendar = () => {
                 />
 
             </section>
+
+            {modalEventAdd && <ModalEventAdd
+                value={''}
+                trueCallback={() => this.handleTrueCallback('modalEventAdd')}
+                falseCallback={() => setModalEventAdd(false)}
+            />}
+
         </React.Fragment>
     );
 };

22ad513d125b1eedbdb7fad2162b6db6fc0ff647
manio4444 <kalinowskixmarcin@o2.pl>
2022-06-14 00:50:55 +0200

    Split Env endpoints into separate .env files

--- src/components/BuildInfo/BuildInfo.js
+++ src/components/BuildInfo/BuildInfo.js
@@ -3,6 +3,6 @@ import preval from 'preval.macro'
 
 const BuildVersion = () => <div>System Version: {process.env.REACT_APP_VERSION}</div>;
 const BuildDate = () => <div>Build Date: {preval`module.exports = new Date().toLocaleString();`}</div>;
-const BuildEnv = () => <div>System Version: {process.env.REACT_APP_ENV}</div>;
+const BuildEnv = () => <div>System Endpoint: {process.env.REACT_APP_ENDPOINT_URL}</div>;
 
 export {BuildVersion, BuildDate, BuildEnv};

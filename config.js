fe934ed9d07e6862742a1e6730de36728884c569
manio4444 <kalinowskixmarcin@o2.pl>
2022-06-14 02:34:47 +0200

    Absolute Imports

---
+++ config.js
@@ -0,0 +1,16 @@
+/*
+Workaround for using absolute paths on WebStorm
+https://stackoverflow.com/questions/54518538/how-to-use-jsconfig-path-with-webstorm
+ */
+
+System.config({
+  paths: {
+    'api/*': './src/api/*',
+    'components/*': './src/components/*',
+    pages: './src/pages',
+    'pages/*': './src/pages/*',
+    router: './src/router',
+    'router/*': './src/router/*',
+    'utils/*': './src/utils/*',
+  },
+});

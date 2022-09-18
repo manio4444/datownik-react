52c7c278f9840ae0ba8df76f6dbb9d970919c243
manio4444 <kalinowskixmarcin@o2.pl>
2022-09-18 23:05:19 +0200

    PageCalendar - birthdays list fetch and adding modal

---
+++ src/components/Calendar/ModalBirthdayAdd.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Button, Header, Icon, Modal, Input, Form } from 'semantic-ui-react';
+import Flatpickr from 'react-flatpickr';
+
+import 'semantic-ui-css/components/modal.min.css';
+import 'semantic-ui-css/components/dimmer.min.css';
+import 'semantic-ui-css/components/header.min.css';
+import 'semantic-ui-css/components/button.min.css';
+import 'semantic-ui-css/components/input.min.css';
+import 'semantic-ui-css/components/form.min.css';
+import './ModalEventAdd.scss';
+
+import { addNewBirthday } from 'components/Calendar/actions';
+
+const FLATPICKR_DATETIME_FORMAT = 'j F Y';
+
+const ModalBirthdayAdd = ({ trueCallback, falseCallback }) => {
+  const [isAdding, setIsAdding] = useState(false);
+  const [text, setText] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleAddButton = () => {
+    setIsAdding(true);
+    addNewBirthday({
+      txt: text,
+      data: date,
+    })
+      .then(
+        (response) => response && trueCallback(response.data.result.newElement)
+      )
+      .catch((error) => console.error(error))
+      .finally(() => setIsAdding(false));
+  };
+
+  return (
+    <Modal open={true} basic size="small" className={'ModalEventAdd'}>
+      <Header icon={'calendar plus'} content={'Dodaj nowe wydarzenie'} />
+      <Modal.Content>
+        <Form inverted>
+          <Form.Field>
+            <label>Tytuł</label>
+            <Input onChange={(e, { value }) => setText(value)} value={text} />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Data</label>
+            <Form.Field inline className={'inline-with-button'}>
+              {/*<input readOnly />*/}
+
+              <Flatpickr
+                value={date}
+                options={{
+                  locale: 'pl',
+                  altInput: true,
+                  altFormat: FLATPICKR_DATETIME_FORMAT,
+                  onChange: (date, value) => setDate(value),
+                }}
+              />
+            </Form.Field>
+          </Form.Field>
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          color="green"
+          inverted
+          onClick={handleAddButton}
+          loading={isAdding}
+          disabled={isAdding}
+        >
+          <Icon name="checkmark" /> Dodaj
+        </Button>
+        <Button
+          color="red"
+          inverted
+          onClick={falseCallback}
+          disabled={isAdding}
+        >
+          <Icon name="remove" /> Anuluj
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
+
+export default ModalBirthdayAdd;
--- src/pages/PageBirthdays/PageBirthdays.js
+++ src/pages/PageBirthdays/PageBirthdays.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Grid, Icon } from 'semantic-ui-react';
 
 import 'semantic-ui-css/components/grid.min.css';
 
+import { getBirthdaysAll } from 'components/Calendar/actions';
+import Placeholder from 'components/Placeholder/Placeholder';
+import ModalBirthdayAdd from 'components/Calendar/ModalBirthdayAdd';
+
 const PageBirthdays = () => {
   const [loading, setLoading] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [modalEventAdd, setModalEventAdd] = useState(false);
 
   useEffect(() => {
     fetchPeriodicEvents();
@@ -11,14 +18,61 @@ const PageBirthdays = () => {
 
   const fetchPeriodicEvents = () => {
     setLoading(true);
-    setLoading(false);
+    getBirthdaysAll()
+      .then((data) => {
+        setEvents(data.data.result);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
   };
 
   return (
     <React.Fragment>
       <section className="calendar">
         <h1>Wydarzenia cykliczne</h1>
+
+        <Grid columns={3}>
+          <Grid.Column />
+
+          <Grid.Column textAlign={'center'}>
+            <Button
+              icon
+              labelPosition="left"
+              onClick={() => setModalEventAdd(true)}
+            >
+              <Icon name="plus" />
+              Dodaj
+            </Button>
+          </Grid.Column>
+
+          <Grid.Column />
+        </Grid>
+
+        <div style={{ margin: '1em 0' }}>
+          {loading && <Placeholder />}
+          {loading && <Placeholder />}
+          {loading && <Placeholder />}
+          {loading && <Placeholder />}
+          {loading && <Placeholder />}
+
+          {!loading &&
+            events.map((event) => (
+              <div key={event.id}>
+                [{event.id}] {event.date} {event.txt}
+              </div>
+            ))}
+        </div>
       </section>
+
+      {modalEventAdd && (
+        <ModalBirthdayAdd
+          trueCallback={() => {
+            setModalEventAdd(false);
+            fetchPeriodicEvents();
+          }}
+          falseCallback={() => setModalEventAdd(false)}
+        />
+      )}
     </React.Fragment>
   );
 };

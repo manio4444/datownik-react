916b0befa6f8ea8127d4b87b2cbf7db96e1d6083
manio4444 <kalinowskixmarcin@o2.pl>
2024-02-12 13:00:43 +0100

    convert MainMenu.js to functional component

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MainMenu.css';
 
-class MainMenu extends Component {
-  state = {
-    opened: false,
+const MainMenu = (props) => {
+  const [opened, setOpened] = useState(false);
+  const refMenu = useRef(null);
+
+  useEffect(() => {
+    toggleBlur();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [opened]);
+
+  useEffect(() => {
+    const handleMousedown = (event) => {
+      if (
+        refMenu.current &&
+        !refMenu.current.contains(event.target) &&
+        opened
+      ) {
+        setOpened(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleMousedown);
+    return () => {
+      document.removeEventListener('mousedown', handleMousedown);
+    };
+  }, [opened]);
+
+  const handleClickElement = () => {
+    toggleState();
   };
 
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  }
-
-  setWrapperRef = (node) => {
-    this.refMenuWrapper = node;
-  };
-
-  handleClickOutside = (event) => {
-    if (
-      this.refMenuWrapper &&
-      !this.refMenuWrapper.contains(event.target) &&
-      this.state.opened
-    ) {
-      this.toggleState();
-    }
-  };
-
-  handleClickElement = () => {
-    this.toggleState();
-  };
-
-  routeElements = [
+  const routeElements = [
     {
       url: '/',
       name: 'Main',
@@ -72,56 +71,51 @@ class MainMenu extends Component {
     },
   ];
 
-  toggleState = () => {
-    this.setState(
-      (prevState) => ({
-        opened: !prevState.opened,
-      }),
-      () => {
-        this.state.opened ? this.props.blurPage() : this.props.unBlurPage();
-      }
-    );
+  const toggleState = () => {
+    setOpened((prevOpened) => !prevOpened);
   };
 
-  getStylesMenu = () => {
+  const toggleBlur = () => {
+    if (opened) {
+      props.blurPage();
+    } else {
+      props.unBlurPage();
+    }
+  };
+
+  const getStylesMenu = () => {
     return {
-      transform: !this.state.opened ? 'translateX(-100%)' : '',
+      transform: !opened ? 'translateX(-100%)' : '',
     };
   };
 
-  render() {
-    return (
-      <div className="MainMenu">
-        <button type="button" className="hamburger" onClick={this.toggleState}>
-          <span className="hamburger_line" />
-        </button>
-        <div
-          className="MainMenuWrapper"
-          style={this.getStylesMenu()}
-          ref={this.setWrapperRef}
-        >
-          <nav>
-            <button
-              type="button"
-              className="hamburger_close"
-              onClick={this.toggleState}
-            >
-              <span className="hamburger_line" />
-            </button>
-            <ul>
-              {this.routeElements.map((link, i) => {
-                return (
-                  <li key={i} onClick={this.handleClickElement}>
-                    <Link to={link.url}>{link.name}</Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </div>
+  return (
+    <div className="MainMenu">
+      <button type="button" className="hamburger" onClick={toggleState}>
+        <span className="hamburger_line" />
+      </button>
+      <div className="MainMenuWrapper" style={getStylesMenu()} ref={refMenu}>
+        <nav>
+          <button
+            type="button"
+            className="hamburger_close"
+            onClick={toggleState}
+          >
+            <span className="hamburger_line" />
+          </button>
+          <ul>
+            {routeElements.map((link, i) => {
+              return (
+                <li key={i} onClick={handleClickElement}>
+                  <Link to={link.url}>{link.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MainMenu;
